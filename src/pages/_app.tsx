4afd5118ce9220f93d10a2ee2fe3096f88f15eba
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-01-23 16:39:26 -0300

    feat: visual structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { globalStyles } from "@/styles/global";
+import { Container, Header } from "@/styles/pages/app";
 import type { AppProps } from "next/app";
+import logoImg from "../assets/Logo.svg";
+import Image from "next/image";
 
 globalStyles(); // This is the line that is causing the error
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="logo" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
-import { styled } from "@/styles";
-
-const Button = styled("button", {
-  backgroundColor: "$black",
-  color: "white",
-});
+import { HomeContainer, Product } from "../styles/pages/home";
+import Image from "next/image";
+import camisa01 from "../assets/camisas/01.png";
+import camisa02 from "../assets/camisas/02.png";
+import camisa03 from "../assets/camisas/03.png";
+import camisa04 from "../assets/camisas/04.png";
 
 export default function Home() {
-  return <Button>Hello World</Button>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camisa01} alt="camisa" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camisa02} alt="camisa" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
