49cc6da2393046274c26ab5997a15fd581fd2bca
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-01-23 10:48:41 -0300

    feat: global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
+import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
+
+globalStyles(); // This is the line that is causing the error
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
